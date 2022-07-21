dd4d233d0842a36a7f585a5df1abf7c3237903e1
ChristopherPH <ChristopherPH@theblackroom.ca>
2022-07-20 23:14:57 -0700

    fix(TheBlackRoom.WinForms): Fix `UIThread()` extension method to perform the action() when the control handle has not been created or throw an exception when not possible

--- TheBlackRoom.WinForms/Extensions/Control/UIThread.cs
+++ TheBlackRoom.WinForms/Extensions/Control/UIThread.cs
@@ -38,21 +38,49 @@ namespace TheBlackRoom.WinForms.Extensions
             if (action is null)
                 throw new ArgumentNullException(nameof(action));
 
-            if (ctrl.IsDisposed)
-                return false;
-
-            if (!ctrl.IsHandleCreated)
-                return false;
-
+            //If InvokeRequired is true, then run the action with invoke. It is possible that
+            //the controls handle has not been created, but InvokeRequired will search parent
+            //controls if needed. See:
+            //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.invokerequired?view=netframework-4.8#remarks
             if (ctrl.InvokeRequired)
             {
                 ctrl.Invoke(action);
-            }
-            else
-            {
-                action();
+                return true;
             }
 
+            //If InvokeRequired is false, we need to ensure the object can still perform the action().
+            //If is possible that the control was created on a different thread but the control's handle has not yet been created.
+
+            //If the control is disposed, any updates will throw an ObjectDisposedException,
+            //so might as well just ignore the action().
+            if (ctrl.IsDisposed)
+                return false;
+
+            //Object has no handle yet. We have a few options:
+            // - Ignore the action(), return false
+            // - Force the handle creation, although this might end up with the control being created
+            //   on the wrong thread. { var force_handle_creation = ctrl.Handle; }
+            //   Note that when referencing the handle property, if the handle has not yet been created,
+            //   referencing this property will force the handle to be created.
+            // - Force the developer to ensure there is a handle on the UI thread, or a parent handle, etc
+            if (!ctrl.IsHandleCreated)
+            {
+                //If we are on the UI thread, then just force the handle creation
+                //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.application.messageloop?view=netframework-4.8
+                if (Application.MessageLoop)
+                {
+                    //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.handle?view=netframework-4.8#remarks
+                    //This might throw an InvalidOperationException for a crossthread exception
+                    var force_handle_creation = ctrl.Handle;
+                }
+                else
+                {
+                    //Force the calling function to ensure there is a valid handle
+                    throw new InvalidOperationException("Control has no handle");
+                }
+            }
+
+            action();
             return true;
         }
     }
