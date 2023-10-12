547345b17671a9096bdf82287cf9cfa36e3359b2
ChristopherPH <ChristopherPH@theblackroom.ca>
2023-10-11 23:21:38 -0700

    fix(EnumDescription): Fix issue when value is not part of the enum

--- TheBlackRoom.System/Extensions/Enum/GetDescription.cs
+++ TheBlackRoom.System/Extensions/Enum/GetDescription.cs
@@ -33,6 +33,9 @@ namespace TheBlackRoom.System.Extensions
         public static string GetDescription(this Enum value)
         {
             var field = value.GetType().GetField(value.ToString());
+            if (field == null)
+                return value.ToString();
+
             var attr = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
 
             return attr?.Description ?? value.ToString();
