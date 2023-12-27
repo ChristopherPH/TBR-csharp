14b7b0c7f3e177120c59026302d131e8f09b1480
ChristopherPH <ChristopherPH@theblackroom.ca>
2023-12-27 14:13:07 -0800

    improve(TheBlackRoom.WinForms): Adjust UIThread() and add options to control behaviour

--- TheBlackRoom/TheBlackRoom.WinForms/Extensions/Control/UIThread.cs
+++ TheBlackRoom/TheBlackRoom.WinForms/Extensions/Control/UIThread.cs
@@ -27,9 +27,21 @@ namespace TheBlackRoom.WinForms.Extensions
     public static class Control_UIThread
     {
         /// <summary>
-        /// Runs a delegate on the UI thread if needed, to avoid cross thread exceptions
+        /// Configurable option to throw exceptions if the action/delegate
+        /// fails to run due to a no valid window handle
         /// </summary>
-        /// <returns>true if delegate called, false if not</returns>
+        public static bool ThrowExceptionsWhenNoHandle { get; set; } = true;
+
+        /// <summary>
+        /// Configurable option to force handle creation when possible
+        /// </summary>
+        public static bool ForceHandleCreation { get; set; } = false;
+
+        /// <summary>
+        /// Runs an action/delegate on the controls creation thread if
+        /// required, as to avoid cross thread exceptions.
+        /// </summary>
+        /// <returns>true if action/delegate called, false if not</returns>
         public static bool UIThread(this Control ctrl, MethodInvoker action)
         {
             if (ctrl is null)
@@ -38,31 +50,56 @@ namespace TheBlackRoom.WinForms.Extensions
             if (action is null)
                 throw new ArgumentNullException(nameof(action));
 
+            //If the control is disposed, any updates will throw an ObjectDisposedException,
+            //so might as well just ignore the action().
+            if (ctrl.IsDisposed)
+                return false;
+
             //If InvokeRequired is true, then run the action with invoke. It is possible that
             //the controls handle has not been created, but InvokeRequired will search parent
             //controls if needed. See:
             //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.invokerequired?view=netframework-4.8#remarks
             if (ctrl.InvokeRequired)
             {
+                //A parent control/form has a handle (as InvokeRequired==true), but the current control does not.
+                //This could be due to the control or its form being invisible.
+                if (!ctrl.IsHandleCreated)
+                {
+                    //We need to invoke, but the current thread is the UI thread. This means the action will run on a thread other
+                    //than the UI thread, meaning the parent control was likely created on the wrong thread.
+                    if (Application.MessageLoop)
+                    {
+                        if (ThrowExceptionsWhenNoHandle)
+                            throw new InvalidOperationException($"Not invoking action on control as its parent is not on the UI thread ({ctrl.Name})");
+                        else
+                            return false;
+                    }
+
+                    //No handle, there isn't much we can really do here. We could fail,
+                    //but it is probably fine to run the action as it will run on the parent
+                    //controls creation thread. We just hope that it ends up on the UI thread
+                    //so the control is created correctly, and that the control plays nicely
+                    //with getting modified when it has no handle.
+                }
+
                 ctrl.Invoke(action);
                 return true;
             }
 
             //If InvokeRequired is false, we need to ensure the object can still perform the action().
-            //If is possible that the control was created on a different thread but the control's handle has not yet been created.
+            //It is possible that the control was created on a different thread from its parent,
+            //but the control's handle has not yet been created.
 
-            //If the control is disposed, any updates will throw an ObjectDisposedException,
-            //so might as well just ignore the action().
-            if (ctrl.IsDisposed)
-                return false;
-
-            //Object has no handle yet. We have a few options:
+            //Object has no handle yet. This could be due to the control or its form being
+            //invisible, or external events causing functions to be run on the control before
+            //the form has fully been created. We have a few options:
             // - Ignore the action(), return false
             // - Force the handle creation, although this might end up with the control being created
-            //   on the wrong thread. { var force_handle_creation = ctrl.Handle; }
+            //   on the wrong thread if not careful
             //   Note that when referencing the handle property, if the handle has not yet been created,
             //   referencing this property will force the handle to be created.
             // - Force the developer to ensure there is a handle on the UI thread, or a parent handle, etc
+            //   via an Exception
             if (!ctrl.IsHandleCreated)
             {
                 //If we are on the UI thread, then just force the handle creation
@@ -70,16 +107,27 @@ namespace TheBlackRoom.WinForms.Extensions
                 if (Application.MessageLoop)
                 {
                     //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.handle?view=netframework-4.8#remarks
-                    //This might throw an InvalidOperationException for a crossthread exception
-                    var force_handle_creation = ctrl.Handle;
+
+                    //We could be nice and create the handle for the control since we are on the UI thread,
+                    //although the control will do it eventually, and we probably don't want to create handles
+                    //when the control isn't visible anyway
+                    if (ForceHandleCreation)
+                    {
+                        //Note that we need to assign the handle to a variable to ensure it does not get optimized out
+                        var force_handle_creation = ctrl.Handle;
+                    }
                 }
                 else
                 {
-                    //Force the calling function to ensure there is a valid handle
-                    throw new InvalidOperationException("Control has no handle");
+                    //Not on UI thread, fail
+                    if (ThrowExceptionsWhenNoHandle)
+                        throw new InvalidOperationException($"Not performing action on control as it has no handle and is not on the UI thread ({ctrl.Name})");
+                    else
+                        return false;
                 }
             }
 
+            //No need to invoke, just run the action
             action();
             return true;
         }
