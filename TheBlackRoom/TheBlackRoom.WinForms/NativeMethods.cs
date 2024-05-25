ec15ec286d5421e26154728a384924cff3c64157
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-05-25 11:57:33 -0700

    feat(TheBlackRoom.WinForms): Add scrolling related constants to native methods

--- TheBlackRoom/TheBlackRoom.WinForms/NativeMethods.cs
+++ TheBlackRoom/TheBlackRoom.WinForms/NativeMethods.cs
@@ -27,10 +27,23 @@ namespace TheBlackRoom.WinForms
     internal class NativeMethods
     {
         [DllImport("user32.dll")]
-        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        public static extern IntPtr SendMessage([In] IntPtr hWnd, [In] uint wMsg,
+            [In] IntPtr wParam, [In] IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage([In] IntPtr hWnd, [In] uint wMsg,
+            [In] IntPtr wParam, [In] int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage([In] IntPtr hWnd, [In] uint wMsg,
+            [In] int wParam, [In] int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage([In] IntPtr hWnd, [In] uint wMsg,
+            [In] IntPtr wParam, ref POINT lParam);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        public static extern bool GetWindowRect([In] IntPtr hWnd, [Out] out RECT lpRect);
 
         [StructLayout(LayoutKind.Sequential)]
         public struct RECT
@@ -40,6 +53,50 @@ namespace TheBlackRoom.WinForms
             public int Right;
             public int Bottom;
         }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SCROLLINFO
+        {
+            public int cbSize;
+            public uint fMask;
+            public int nMin;
+            public int nMax;
+            public uint nPage;
+            public int nPos;
+            public int nTrackPos;
+        }
+
+        public enum ScrollInfoMask : uint
+        {
+            SIF_RANGE = 0x1,
+            SIF_PAGE = 0x2,
+            SIF_POS = 0x4,
+            SIF_DISABLENOSCROLL = 0x8,
+            SIF_TRACKPOS = 0x10,
+            SIF_ALL = (SIF_RANGE | SIF_PAGE | SIF_POS | SIF_TRACKPOS),
+        }
+
+        /// <summary>
+        /// Scroll Bar Orientation
+        /// </summary>
+        public enum SBOrientation : int
+        {
+            SB_HORZ = 0x0,
+            SB_VERT = 0x1,
+            SB_CTL = 0x2,
+            SB_BOTH = 0x3
+        }
+
+        public const int WM_USER = 0x0400;
+
+        public const int WM_SETREDRAW = 0x000B;
 
         public const int WM_MOUSEACTIVATE = 0x0021;
         public const int WM_CONTEXTMENU = 0x007B;
@@ -70,9 +127,31 @@ namespace TheBlackRoom.WinForms
         public const int LVM_SETICONSPACING = LVM_FIRST + 53;
         public const int LVM_SETVIEW = LVM_FIRST + 142;
 
+        /* RichTextBox Constants
+         * https://referencesource.microsoft.com/#system.windows.forms/winforms/managed/system/winforms/RichTextBoxConstants.cs
+         */
+
+        public const int EM_GETEVENTMASK = NativeMethods.WM_USER + 59;
+        public const int EM_SETEVENTMASK = NativeMethods.WM_USER + 69;
+
+        // Message for getting and restoring scroll pos
+        public const int EM_GETSCROLLPOS = NativeMethods.WM_USER + 221;
+        public const int EM_SETSCROLLPOS = NativeMethods.WM_USER + 222;
+
+
+        /* Scrolling */
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetScrollInfo([In] IntPtr hWnd, int nBar,
+            ref SCROLLINFO lpsi);
+
+        [DllImport("user32.dll")]
+        public static extern int SetScrollInfo([In] IntPtr hWnd, int nBar,
+            ref SCROLLINFO lpsi, bool redraw);
+
         /* Misc */
         [DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
-        public extern static int SetWindowTheme(IntPtr hWnd, string pszSubAppName,
-            string pszSubIdList);
+        public extern static int SetWindowTheme([In] IntPtr hWnd,
+            string pszSubAppName, string pszSubIdList);
     }
 }
