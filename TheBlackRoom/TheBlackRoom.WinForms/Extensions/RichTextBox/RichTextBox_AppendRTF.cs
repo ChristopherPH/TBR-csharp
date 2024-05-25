f229c20eed5fd489f5d4b0497a9da92dfd4502e9
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-05-25 12:24:32 -0700

    feat(TheBlackRoom.WinForms): Update AppendRTF to take flag to determine if the control should be scrolled or not

--- TheBlackRoom/TheBlackRoom.WinForms/Extensions/RichTextBox/RichTextBox_AppendRTF.cs
+++ TheBlackRoom/TheBlackRoom.WinForms/Extensions/RichTextBox/RichTextBox_AppendRTF.cs
@@ -20,6 +20,7 @@
  * SOFTWARE.
  */
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TheBlackRoom.WinForms.Extensions
@@ -30,10 +31,29 @@ namespace TheBlackRoom.WinForms.Extensions
         /// Appends RTF text to the current RTF of a rich text box, and scrolls
         /// the rich text box to the end
         /// </summary>
-        public static void AppendRtf(this RichTextBox richTextBox, string Rtf)
+        public static void AppendRtf(this RichTextBox richTextBox, string Rtf, bool scrollToEnd = true)
         {
+            if (richTextBox == null)
+                return;
+
             if (string.IsNullOrEmpty(Rtf))
                 return;
+
+            //Variables used to save selections and positions when not scrolling
+            int selectionStart = 0, selectionLength = 0;
+            Point scrollPosition = Point.Empty;
+
+            //Save selection and scroll positions
+            if (!scrollToEnd)
+            {
+                selectionStart = richTextBox.SelectionStart;
+                selectionLength = richTextBox.SelectionLength;
+                scrollPosition = richTextBox.GetScrollPosition();
+            }
+
+            //Disable drawing, so that changing the selections doesn't adjust
+            //the current position
+            richTextBox.DisableRedraw();
 
             //put selection at the end of the text as that is where
             //the RTF will be inserted
@@ -50,9 +70,22 @@ namespace TheBlackRoom.WinForms.Extensions
             }
             finally
             {
-                //put selection at the end of the text, scrolling the rich text box
-                richTextBox.Select(richTextBox.TextLength, 0);
+                if (scrollToEnd)
+                {
+                    //put selection at the end of the text, scrolling the rich text box
+                    richTextBox.Select(richTextBox.TextLength, 0);
+                    richTextBox.ScrollToBottom();
+                }
+                else
+                {
+                    //restore selection and scroll position
+                    richTextBox.Select(selectionStart, selectionLength);
+                    richTextBox.SetScrollPosition(scrollPosition);
+                }
             }
+
+            //Restore drawing with the current position
+            richTextBox.EnableRedraw(true);
         }
     }
 }
