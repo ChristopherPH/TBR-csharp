27fd23e5268255fd90a17e517651403ca9df517b
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-05-28 08:23:22 -0700

    feat(TheBlackRoom.Gfx): Add TheBlackRoom.Gfx with methods to create and draw rounded rectangles

---
+++ TheBlackRoom/TheBlackRoom.Gfx/Extensions/Graphics/Graphics_RoundedRect.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Copyright (c) 2024 Christopher Hayes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+using System.Drawing;
+using TheBlackRoom.Gfx.Utility;
+
+namespace TheBlackRoom.Gfx.Extensions
+{
+    public static class Graphics_RoundedRect
+    {
+        /// <summary>
+        /// Draws a rounded rectangle
+        /// </summary>
+        /// <param name="graphics">Graphics object to draw upon</param>
+        /// <param name="pen">Pen that determines the color, width, and style of the rectangle</param>
+        /// <param name="rectangle">Bounding box of rectangle</param>
+        /// <param name="radius">Corner radius</param>
+        public static void DrawRoundedRectangle(this Graphics graphics, Pen pen,
+            RectangleF rectangle, float radius)
+        {
+            if ((graphics == null) || (pen == null))
+                return;
+
+            using (var rr = RoundedRectangles.CreateRoundedRectangle(rectangle, radius))
+                graphics.DrawPath(pen, rr);
+        }
+
+        /// <summary>
+        /// Fills a rounded rectangle
+        /// </summary>
+        /// <param name="graphics">Graphics object to draw upon</param>
+        /// <param name="brush">Brush that determines the characteristics of the fill</param>
+        /// <param name="rectangle">Bounding box of rectangle</param>
+        /// <param name="radius">Corner radius</param>
+        public static void FillRoundedRectangle(this Graphics graphics, Brush brush,
+            RectangleF rectangle, float radius)
+        {
+            if ((graphics == null) || (brush == null))
+                return;
+
+            using (var rr = RoundedRectangles.CreateRoundedRectangle(rectangle, radius))
+                graphics.FillPath(brush, rr);
+        }
+    }
+}
---
+++ TheBlackRoom/TheBlackRoom.Gfx/Utility/RoundedRectangles.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Copyright (c) 2024 Christopher Hayes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TheBlackRoom.Gfx.Utility
+{
+    public static class RoundedRectangles
+    {
+        /// <summary>
+        /// Creates a graphics path representing a rectangle with rounded corners
+        /// </summary>
+        /// <param name="rectangle">Bounding box of rectangle</param>
+        /// <param name="radius">Corner radius</param>
+        /// <returns>A GraphicsPath object (which must be disposed after use)</returns>
+        public static GraphicsPath CreateRoundedRectangle(RectangleF rectangle, float radius)
+        {
+            var path = new GraphicsPath();
+
+            /* No valid radius, return the rectangle */
+            if (radius <= 0)
+            {
+                path.AddRectangle(rectangle);
+                return path;
+            }
+
+            var diameter = radius * 2.0f;
+            var arc = new RectangleF(rectangle.X, rectangle.Y, diameter, diameter);
+
+            /* Start with top left */
+            path.AddArc(arc, 180, 90);
+
+            /* Add top right */
+            arc.X = rectangle.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            /* Add bottom right */
+            arc.Y = rectangle.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            /* Add bottom left */
+            arc.X = rectangle.Left;
+            path.AddArc(arc, 90, 90);
+
+            /* Close */
+            path.CloseFigure();
+
+            return path;
+        }
+    }
+}
